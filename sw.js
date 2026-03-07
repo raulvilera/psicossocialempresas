4d5935e6f0cf1dc211658b6f14c240b2d7eb18a8
Raul <raulvilera@gmail.com>
2026-03-07 10:08:22 -0300

    Fix PWA: Correct JS syntax and add SW registration to all pages (v3.6.1)

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
-const CACHE_NAME = 'psicos-v3.6.0';
+const CACHE_NAME = 'psicos-v3.6.1';
 const ASSETS = [
     '/login',
     '/api/version'
 ];
 
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
